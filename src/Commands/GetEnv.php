bc38fe08dfe5f87fccad548e3dfe5e572b9a2765
Kaan Kilic <bl4cksta@gmail.com>
2019-06-10 16:26:49 +0300

    Initial Commit

---
+++ src/Commands/GetEnv.php
@@ -0,0 +1,22 @@
+
+<?php namespace Kaankilic\PlatformCreator\Commands;
+/**
+ * Class GetEnvironmentFile
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
+class GetEnv
+{
+    /**
+     * Handle the command.
+     *
+     * @param Application $application
+     * @return string
+     */
+    public function handle()
+    {
+		return base_path('.env');
+    }
+}
---
+++ src/Providers/PlatformCreatorServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Kaankilic\PlatformCreator\Providers;
+use Illuminate\Support\ServiceProvider;
+class ServerUpServiceProvider extends ServiceProvider {
+	protected $defer = false;
+	/**
+	* Bootstrap the application services.
+	*
+	* @return void
+	*/
+	public function boot(\Illuminate\Routing\Router $router){
+		$this->publishes([
+			__DIR__.'/../../config/serverup.php' => config_path('serverup.php')
+		]);
+		$this->app->bind('ServerUp', 'Kaankilic\ServerUp\Libraries\ServerUp' );
+	}
+
+	/**
+	* Register the application services.
+	*
+	* @return void
+	*/
+	public function register(){
+		$this->mergeConfigFrom(__DIR__ . '/../../config/serverup.php', 'serverup');
+		return array('ServerUp');
+	}
+}
